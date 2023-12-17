e0bf778bc95388856fc167d2b60c0e9b4496f8a3
mrghodadara <vgghodadara2003@gmail.com>
2023-12-17 15:38:53 +0530

    Working on responsive

--- src/components/card/ServiceCard.tsx
+++ src/components/card/ServiceCard.tsx
@@ -9,14 +9,12 @@ interface ServiceCardProps {
 
 const ServiceCard = ({ icon, name, description }: ServiceCardProps) => {
   return (
-    <div className="flex cursor-pointer select-none flex-col items-start gap-5 rounded-lg bg-white/30 p-14 px-11 transition-all duration-500 hover:-translate-y-3 hover:bg-serviceHover">
+    <div className="flex cursor-pointer select-none flex-col items-start gap-5 rounded-lg bg-white/30 px-6 py-8 transition-all duration-500 hover:-translate-y-3 hover:bg-serviceHover md:p-10 lg:px-11 lg:py-14">
       <div className="mb-1 text-white">{icon}</div>
       <h3 className="font-poppins text-xl font-medium leading-6 text-white">
         {name}
       </h3>
-      <p className="font-poppins text-lg leading-7 text-white ">
-        {description}
-      </p>
+      <p className="font-poppins text-lg leading-7 text-white">{description}</p>
     </div>
   );
 };
